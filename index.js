c9f720f14e160b894936b232284d1a653f14e551
Ben Coyne <bencoyne123@yahoo.co.uk>
2022-03-23 15:37:57 +0000

    (wip) adding functionality to change genre of drums

--- index.js
+++ index.js
@@ -17,3 +17,24 @@ function removeTransition(e) {
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 window.addEventListener("keydown", playSound);
+
+const jazz = document.querySelector("#jazz");
+jazzDrums = [
+	"kick",
+	"snare",
+	"brush",
+	"tom1",
+	"tom2",
+	"closed hat",
+	"open hat",
+	"ride",
+	"crash",
+];
+jazz.addEventListener("click", () => {
+	document.body.style.backgroundImage =
+		"linear-gradient(black, black), url('https://cdn.mos.cms.futurecdn.net/378274d88018606e7f61e161f9172126.jpg')";
+	drumNames = document.querySelectorAll(".drum");
+	for (let i = 0; i < drumNames.length; i++) {
+		drumNames[i].textContent = jazzDrums[i];
+	}
+});
